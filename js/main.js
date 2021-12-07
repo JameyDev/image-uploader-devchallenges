df4c13bfee39df7aff6b5e6e0759c61f899130bb
Jamey Bryce <jybryce4@gmail.com>
2021-12-07 13:30:36 -0600

    completed green border functionality

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const initApp = function () {
+    const imgDropArea = document.querySelector('.img-drop-area');
+
+    const active = () => imgDropArea.classList.add('green-border');
+
+    const inActive = () => imgDropArea.classList.remove('green-border');
+
+    const preventDef = (ev) => ev.preventDefault();
+
+    const events = ['dragenter', 'dragover', 'dragleave', 'drop'];
+    events.forEach(eventName => {
+        imgDropArea.addEventListener(eventName, preventDef);
+    });
+
+    const dragInto = [events[0], events[1]];
+    dragInto.forEach(eventName => {
+        imgDropArea.addEventListener(eventName, active);
+    });
+
+    const dragNDrop = [events[2], events[3]];
+    dragNDrop.forEach(eventName => {
+        imgDropArea.addEventListener(eventName, inActive);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", initApp);
